ee3b386c2b4611843c511f7620d7ab99afdce59c
Mikhail Pridushchenko <mikhail.pridushchenko@dsr-corporation.com>
2021-01-27 14:10:33 +0300

    OpDataSet operations must set corresponding network fields

--- src/app/commissioner_app.cpp
+++ src/app/commissioner_app.cpp
@@ -663,7 +663,7 @@ exit:
     return error;
 }
 
-Error CommissionerApp::GetPanId(uint16_t &aPanId)
+Error CommissionerApp::GetPanId(PanId &aPanId)
 {
     Error error;
 
@@ -679,7 +679,7 @@ exit:
     return error;
 }
 
-Error CommissionerApp::SetPanId(uint16_t aPanId, MilliSeconds aDelay)
+Error CommissionerApp::SetPanId(PanId aPanId, MilliSeconds aDelay)
 {
     Error                     error;
     PendingOperationalDataset pendingDataset;
